19b70082373ada7b76bff013fc64978af58557ef
Anderson Nishihara <nishihara@Raijin.local>
2013-11-21 20:45:08 -0200

    Uses log so that number cannot get so small

--- classifier.py
+++ classifier.py
@@ -1,28 +1,26 @@
 import training_data
 import re
+import math
 
 class Classifier:
 	def classify(self,text,prior=0.5,c=10e-6):
 		""" Remove a pontuacao do texto """
 		words =  re.findall(r"[\w']+",text)
 		"""words = text.split()"""
-		print words
 		data = training_data.TrainingData()
-		spamLikehood = 1
-		hamLikehood = 1
+		spamLikehood = math.log(1)
+		hamLikehood = math.log(1)
 		for word in words:
-			print spamLikehood
 			""" Calculo de spam"""
 			if word in data.spam:
-				spamLikehood *= data.spam[word]
+				spamLikehood += math.log(data.spam[word])
 			else:
-				spamLikehood *= c
+				spamLikehood += math.log(c)
 			""" Calculo de ham"""
 			if word in data.ham:
-				hamLikehood *= data.ham[word]
+				hamLikehood += math.log(data.ham[word])
 			else:
-				hamLikehood *= c
-		isSpam = spamLikehood * prior
-		isHam = hamLikehood * prior
-		print "HAM:", isHam
-		print "SPAM:", isSpam
+				hamLikehood += math.log(c)
+		isSpam = spamLikehood + math.log(prior)
+		isHam = hamLikehood +  math.log(prior)
+		print "HAM:", isHam, "SPAM:", isSpam
