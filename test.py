5005972dddefce3edae39597e28dadf8a69d418e
Anderson Nishihara <nishihara@Raijin.local>
2013-11-21 21:00:37 -0200

    Uses other datas to test

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import classifier
+import training_data
 import os
 
 def extractText(filepath):
@@ -6,15 +7,33 @@ def extractText(filepath):
 	text = file.read()
 	lines = text.split("\n")
 	return "\n".join(lines[lines.index("")+1:])
-		
+
+def test(dirName,classificador,trainingData):
+	easy_ham = os.listdir(dirName)
+	if "cmds" in easy_ham : 
+		easy_ham.remove("cmds")
+	spamCount = 0;
+	hamCount = 0;
+	for file in easy_ham:
+		text = extractText(dirName+"/"+file)
+		spamProb = classificador.classify(text,trainingData.spam)
+		hamProb = classificador.classify(text,trainingData.ham)
+		if spamProb>hamProb: 
+			spamCount += 1
+		else: 
+			hamCount += 1
+	print dirName
+	print "SPAM:",spamCount," HAM:",hamCount	
 
 classificador = classifier.Classifier()
-easy_ham = os.listdir("easy_ham")
-if "cmds" in easy_ham : 
-	easy_ham.remove("cmds")
-easy_ham = easy_ham[1:2]
-for file in easy_ham:
-	text = extractText("easy_ham/"+file)
-	classificador.classify("what text can I test")
+trainingData = training_data.TrainingData()
+test("easy_ham",classificador,trainingData)
+test("easy_ham_2",classificador,trainingData)
+test("hard_ham",classificador,trainingData)
+test("hard_ham_2",classificador,trainingData)
+test("spam",classificador,trainingData)
+test("spam_2",classificador,trainingData)
+
+	
 	
 	
